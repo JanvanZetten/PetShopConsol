208b4ac922e61c892041b0bbb09c98b474af70b1
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-08-29 10:37:58 +0200

    firstCommit
    the basic Clean architecture

---
+++ EASV.PetShopConsol.Menu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EASV.PetShopConsol.Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
