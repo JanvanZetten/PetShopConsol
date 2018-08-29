5be92e176a5c62fa7ff032c331d1d436473c54fa
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-08-29 10:56:02 +0200

    Pet entity and enum for animal type

---
+++ EASV.PetShopConsol.Core.Entity/AnimalType.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace EASV.PetShopConsol.Core.Entity
+{
+    public enum AnimalType
+    {
+        Dog, Cat, Rabbit, Snake, Goat, Pony, Fish, Bird, Other
+    }
+}
---
+++ EASV.PetShopConsol.Core.Entity/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EASV.PetShopConsol.Core.Entity
+{
+    public class Pet
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public AnimalType Type { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime SoldDate { get; set; }
+        public string Color { get; set; }
+        public string PreviousOwner { get; set; }
+        public double Price { get; set; }
+
+        public Pet(int Id)
+        {
+            this.Id = Id;
+        }
+
+    }
+}
