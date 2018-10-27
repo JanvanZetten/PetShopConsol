367b2bec1410fafe71095060a2a7e7c95979290b
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-10-27 12:19:09 +0200

    User with id

--- EASV.PetShopConsol.Core.Entity/User.cs
+++ EASV.PetShopConsol.Core.Entity/User.cs
@@ -6,7 +6,7 @@ namespace EASV.PetShopConsol.Core.Entity
         public User()
         {
         }
-
+        public long Id { get; set; }
         public String Password { get; set; }
         public String Username { get; set; }
         public bool IsAdmin { get; set; }
